20f09bf6048670844a36904bd70c6af77783e571
Cesaire Joris Kuete Fouodo <fouodo@imbs.uni-luebeck.de>
2023-12-11 13:07:07 +0100

    Fix setting for testing mode

--- R-code/01-scenario1/01-vita-jobs.R
+++ R-code/01-scenario1/01-vita-jobs.R
@@ -54,44 +54,14 @@ hyperparam_settings <- hyperparam_settings[!(sample.fraction == 1 &
 
 ## Just some replicates if the system is in the testing mode, and 100 otherwise.
 seed <- if(testing_mode){
-  ## Variation of min.node.size
-  nodesize.prop.var <- data.frame(nodesize.prop = nodesize.prop,
-                                  no.threads = no.threads,
-                                  replace = TRUE,
-                                  sample.fraction = 0.632,
-                                  mtry = 0.014,
-                                  num.trees = num.trees,
-                                  holdout = holdout)
-  ## Variation of replace
-  replace.var <- data.frame(nodesize.prop = 0.01,
-                            no.threads = no.threads,
-                            replace = replace,
-                            sample.fraction = 0.632,
-                            mtry = 0.014,
-                            num.trees = num.trees,
-                            holdout = holdout)
-  ## Variation of sample.fraction
-  sample.fraction.var <- data.frame(nodesize.prop = 0.01,
+  ## Variation of mtry
+  hyperparam_settings <- data.frame(nodesize.prop = 0.01,
                                     no.threads = no.threads,
                                     replace = TRUE,
-                                    sample.fraction = sample.fraction,
-                                    mtry = 0.014,
+                                    sample.fraction = 0.632,
+                                    mtry = mtry,
                                     num.trees = num.trees,
                                     holdout = holdout)
-  ## Variation of mtry
-  mtry.var <- data.frame(nodesize.prop = 0.01,
-                         no.threads = no.threads,
-                         replace = TRUE,
-                         sample.fraction = 0.632,
-                         mtry = mtry,
-                         num.trees = num.trees,
-                         holdout = holdout)
-  hyperparam_settings <- data.table::rbindlist(list(
-    nodesize.prop.var,
-    replace.var,
-    sample.fraction.var,
-    mtry.var
-  ))
   1:5
 } else {
   1:100
